bbf49e4726e4b23719c53a45854198b3f03735db
fsbolgi <francesca.sbolgi@gmail.com>
2019-09-09 09:09:27 +0200

    Refactor main into stages

--- sequential/main.cpp
+++ sequential/main.cpp
@@ -1,23 +1,15 @@
-#include "./util.hpp"
+#include "./stages.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    // read parameters
-    if (argc < 7)
+    if (check_parameters(argc, argv))
     {
-        cout << "Parameters are: 1) Stream length (n)\n"
-             << "2) Tuple size (m)\n"
-             << "3) Window size (w)\n"
-             << "4) Sliding factor (k)\n"
-             << "5) Seed (s)"
-             << "6) Verbose (0 = false)" << endl;
         return -1;
     }
 
-    /* TODO: check if parameters are positive integers */
-    /* TODO: stream length must be > than window */
+    // Store parameters
     int n = atoi(argv[1]);
     int m = atoi(argv[2]);
     int w = atoi(argv[3]);
@@ -25,57 +17,59 @@ int main(int argc, char *argv[])
     int s = atoi(argv[5]);
     int verbose = atoi(argv[6]);
 
+    // Decide seed for random numbers
     srand(s);
 
-    /* TODO: use type T */
+    // The window is stored in the queue
     deque<int *> window;
-    deque<pair<int *, int>> DB_rest;
-    deque<pair<int *, int>> DB_sky;
+    // The current skyline is saved as a linked list
+    Node *DB_sky = NULL;
+    // Another linked list is used to store tuples that may end up
+    // in the skyline later
+    Node *DB_rest = NULL;
 
+    // For each element in the stream
     for (int i = 0; i < n; i++)
     {
-        int *t = generate_tuple(m);
+        int *t = generate_tuple(m, verbose);
         if (verbose)
         {
             window.push_front(t);
         }
-        bool sky = true;
         int expire = i + w;
-        int sky_pos = 0;
-        for (pair<int *, int> t_sky : DB_sky)
-        {
-            if (dominate(t, t_sky.first, m))
-            {
-                //DB_sky.push_front(make_pair(t, i + w));
-                DB_sky.erase(DB_sky.begin() + sky_pos);
-            }
-            if (dominate(t_sky.first, t, m))
-            {
-                sky = false;
-                expire = t_sky.second;
-            }
-            sky_pos++;
-        }
+        int insert = 0;
+
+        // Check each element in the skyline list
+        bool sky = check_skyline(i, t, &DB_sky, m, &insert);
+
+        // Check each element in the Rest list
+        check_rest(i, t, &DB_rest, &DB_sky, m, &insert, expire);
+
+        // New tuple is inserted either in the skyline or in the rest list
         if (sky)
         {
-            DB_sky.push_front(make_pair(t, expire));
+            push_node(&DB_sky, t, expire, insert);
         }
         else
         {
-            DB_rest.push_front(make_pair(t, expire));
+            push_node(&DB_rest, t, expire, insert);
         }
-        // wait til up to speed
-        if (i >= w - 1 && verbose)
-        {
-            cout << "WINDOW: ";
-            print_window(window, m);
-            cout << "SKYLINE: ";
-            print_skyline(DB_sky, m);
 
+        // Wait until correct window and print only when necessary
+        if ((i >= w - 1) && verbose)
+        {
+            if ((i + 1 - w) % k == 0)
+            {
+                cout << "WINDOW: ";
+                print_queue(window, m);
+                cout << "SKYLINE: ";
+                print_node_list(DB_sky, m);
+                cout << "REST: ";
+                print_node_list(DB_rest, m);
+                cout << endl;
+            }
             window.pop_back();
-            //
         }
     }
-
     return 0;
 }
